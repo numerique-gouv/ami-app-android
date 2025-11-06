b9d3009f9d53192849761209c1824c70d2c5f886
Aline Bonnet <aline.bonnet@otterways.com>
2025-11-06 10:39:48 +0100

    add post notification permission

--- app/src/main/java/fr/gouv/ami/dev/home/HomeScreen.kt
+++ app/src/main/java/fr/gouv/ami/dev/home/HomeScreen.kt
@@ -1,22 +1,61 @@
 package fr.gouv.ami.dev.home
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.content.res.Configuration
+import android.os.Build
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
 import fr.gouv.ami.dev.api.baseUrl
 import fr.gouv.ami.dev.ui.theme.AMITheme
 
 @SuppressLint("SetJavaScriptEnabled")
 @Composable
 fun HomeScreen() {
+
+    val context = LocalContext.current
+
+    /**Check notification permission **/
+
+    val notificationPermissionGranted = remember {
+        mutableStateOf(
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                ContextCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) == PackageManager.PERMISSION_GRANTED
+            } else true
+        )
+    }
+
+    val notificationPermissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        notificationPermissionGranted.value = isGranted
+    }
+
+    LaunchedEffect(Unit) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
+            notificationPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+    }
+
+
+    /** UI **/
 
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
         AndroidView(
