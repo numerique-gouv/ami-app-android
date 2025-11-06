a247fb44da3586eefda6d9377b3af7621ea506a1
Aline Bonnet <aline.bonnet@otterways.com>
2025-11-06 17:22:54 +0100

    check notification permission when the boot completed

--- app/src/main/java/fr/gouv/ami/dev/notifications/BootCompleted.kt
+++ app/src/main/java/fr/gouv/ami/dev/notifications/BootCompleted.kt
@@ -1,13 +1,27 @@
 package fr.gouv.ami.dev.notifications
 
+import android.Manifest
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
 
 class BootCompleted : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent?) {
         if (intent?.action == Intent.ACTION_BOOT_COMPLETED) {
-            context.startForegroundService(Intent(context, NotificationService::class.java))
+            if (ContextCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) == PackageManager.PERMISSION_GRANTED
+                &&
+                ContextCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                context.startForegroundService(Intent(context, NotificationService::class.java))
+            }
         }
     }
 }
