ad9247a0629f9ed34b205894762de44e00cf442e
Aline Bonnet <aline.bonnet@otterways.com>
2025-12-01 10:59:19 +0100

    add reviewApps api

---
+++ app/src/main/java/fr/gouv/ami/data/repository/ReviewAppsFlow.kt
@@ -0,0 +1,13 @@
+package fr.gouv.ami.data.repository
+
+import fr.gouv.ami.api.apiService
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.Response
+
+fun getReviewApps() : Flow<Response<List<List<String>>>> {
+    return flow {
+        val response = apiService.getReviewApps()
+        emit(response)
+    }
+}
