2690a2bea2bcdf0cfc6d737af4466604979617fe
Aline Bonnet <aline.bonnet@otterways.com>
2025-12-12 17:41:24 +0100

    add back button for external process

--- app/src/main/java/fr/gouv/ami/home/HomeScreen.kt
+++ app/src/main/java/fr/gouv/ami/home/HomeScreen.kt
@@ -5,23 +5,29 @@ import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.content.res.Configuration
 import android.os.Build
+import android.util.Log
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import fr.gouv.ami.api.baseUrl
+import fr.gouv.ami.components.BackBar
 import fr.gouv.ami.ui.theme.AMITheme
 
 @SuppressLint("SetJavaScriptEnabled")
@@ -29,6 +35,8 @@ import fr.gouv.ami.ui.theme.AMITheme
 fun HomeScreen() {
 
     val context = LocalContext.current
+    var hasBackBar by remember { mutableStateOf(false) }
+    var currentUrl by remember { mutableStateOf(baseUrl) }
 
     /**Check notification permission **/
 
@@ -58,20 +66,49 @@ fun HomeScreen() {
     /** UI **/
 
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        AndroidView(
+        Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(innerPadding),
-            factory = {
-                WebView(it).apply {
-                    settings.javaScriptEnabled = true
-                    settings.allowFileAccess = true
-                    settings.allowContentAccess = true
-                    settings.domStorageEnabled = true
-                    webViewClient = WebViewClient()
-                    loadUrl(baseUrl)
+                .padding(innerPadding)
+        ) {
+            if (hasBackBar) {
+                BackBar {
+                    currentUrl = baseUrl
                 }
-            })
+            }
+            AndroidView(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f),
+                factory = {
+                    WebView(it).apply {
+                        settings.javaScriptEnabled = true
+                        settings.allowFileAccess = true
+                        settings.allowContentAccess = true
+                        settings.domStorageEnabled = true
+                        webViewClient = object : WebViewClient() {
+                            override fun doUpdateVisitedHistory(
+                                view: WebView?,
+                                url: String?,
+                                isReload: Boolean
+                            ) {
+                                hasBackBar = !url.isNullOrEmpty() && !url.contains(baseUrl)
+                                Log.d("HomeScreen", url!!)
+                                currentUrl = url
+
+                                super.doUpdateVisitedHistory(view, url, isReload)
+                            }
+                        }
+                        loadUrl(baseUrl)
+                    }
+                },
+                update = { webView ->
+                    if (webView.url != currentUrl) {
+                        webView.loadUrl(currentUrl)
+                    }
+                }
+            )
+        }
     }
 }
 
