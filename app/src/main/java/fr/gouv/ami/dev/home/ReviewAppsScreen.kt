dd9228c4d6978fdf9457055e5dc64feab85ae1ca
Mathieu Agopian <mathieu@agopian.info>
2025-12-24 15:58:23 +0100

    Simplify the review apps list query from the backend

--- app/src/main/java/fr/gouv/ami/dev/home/ReviewAppsScreen.kt
+++ app/src/main/java/fr/gouv/ami/dev/home/ReviewAppsScreen.kt
@@ -35,22 +35,20 @@ fun ReviewAppsScreen(onSelectedReviewApp: () -> Unit) {
     var reviews by remember {
         mutableStateOf<MutableList<Review>?>(null)
     }
-    val stagingString = stringResource(R.string.staging)
 
     LaunchedEffect(Unit) {
         val reviewFlow = getReviewApps()
         reviewFlow
             .catch { e ->
-                Log.d("ReleasePickerScreen", e.toString())
+                Log.e("ReviewAppsScreen", "Error fetching review apps", e)
             }
-            .collect {
-                val mainReview: Review =
-                    Review(url = "https://ami-back-staging.osc-fr1.scalingo.io",
-                        title = stagingString,
-                        number = "0",
-                        description = stagingString)
-                reviews = it.body()
-                reviews?.add(0, mainReview)
+            .collect { response ->
+                if (response.isSuccessful) {
+                    reviews = response.body()
+                    Log.d("ReviewAppsScreen", "Successfully loaded ${reviews?.size ?: 0} review apps")
+                } else {
+                    Log.e("ReviewAppsScreen", "Error loading review apps: ${response.code()} - ${response.message()}")
+                }
             }
     }
 
