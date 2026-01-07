70694b54036c410ad5e0323c6858b54e6de0c564
Aline Bonnet <aline.bonnet@otterways.com>
2026-01-07 11:08:22 +0100

    reload webview when connection is restored

--- app/src/main/java/fr/gouv/ami/home/WebViewViewModel.kt
+++ app/src/main/java/fr/gouv/ami/home/WebViewViewModel.kt
@@ -3,10 +3,10 @@ package fr.gouv.ami.home
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.lifecycle.ViewModel
 import fr.gouv.ami.api.baseUrl
+import fr.gouv.ami.global.BaseViewModel
 
-class WebViewViewModel : ViewModel() {
+class WebViewViewModel : BaseViewModel() {
     var currentUrl by mutableStateOf(baseUrl)
     var lastUrl by mutableStateOf(baseUrl) //not used for now
 
