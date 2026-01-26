206ddb88badca31894bb2cbcf0afd6a7c1cb1206
Aline Bonnet <aline.bonnet@otterways.com>
2026-01-26 13:43:48 +0100

    add settings screen

---
+++ app/src/main/java/fr/gouv/ami/settings/SettingsScreen.kt
@@ -0,0 +1,85 @@
+package fr.gouv.ami.settings
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import fr.gouv.ami.R
+import fr.gouv.ami.global.BaseScreen
+import fr.gouv.ami.ui.theme.AMITheme
+import fr.gouv.ami.ui.theme.BlueFrance625
+
+@Composable
+fun SettingsScreen() {
+
+    var checked by remember { mutableStateOf(true) }
+
+    BaseScreen(
+        hasTopBar = true,
+        topBarTitle = stringResource(R.string.settings),
+        viewModel = viewModel()
+    ) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Text(
+                    stringResource(R.string.settings_notification),
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(end = 8.dp)
+                )
+                Switch(
+                    checked = checked,
+                    onCheckedChange = {
+                        checked = it
+                    },
+                    colors = SwitchDefaults.colors(
+                        checkedThumbColor = Color.White,
+                        checkedTrackColor = MaterialTheme.colorScheme.primary
+                    )
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewSettingsScreenLight() {
+    AMITheme {
+        SettingsScreen()
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun PreviewSettingsScreenDark() {
+    AMITheme {
+        SettingsScreen()
+    }
+}
