de059771ba01435249d8ad45ff267417575fd609
Aline Bonnet <aline.bonnet@otterways.com>
2026-01-29 11:18:13 +0100

    enable/disable notification permission

--- app/src/main/java/fr/gouv/ami/settings/SettingsScreen.kt
+++ app/src/main/java/fr/gouv/ami/settings/SettingsScreen.kt
@@ -6,35 +6,56 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
 import fr.gouv.ami.R
 import fr.gouv.ami.global.BaseScreen
+import fr.gouv.ami.home.NotificationPermissionHandler
+import fr.gouv.ami.home.WebViewViewModel
+import fr.gouv.ami.home.isPermissionGranted
 import fr.gouv.ami.ui.theme.AMITheme
-import fr.gouv.ami.ui.theme.BlueFrance625
 
 @Composable
-fun SettingsScreen(onBackButton: () -> Unit) {
+fun SettingsScreen(webViewViewModel: WebViewViewModel, onBackButton: () -> Unit) {
 
-    var checked by remember { mutableStateOf(true) }
+    NotificationPermissionHandler(webViewViewModel)
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    var checked by remember { mutableStateOf(isPermissionGranted(context)) }
+
+    // check notification permission when activity status is onResume
+    DisposableEffect(lifecycleOwner) {
+        val observer = LifecycleEventObserver { _, event ->
+            if (event == Lifecycle.Event.ON_RESUME) {
+                checked = isPermissionGranted(context)
+            }
+        }
+
+        lifecycleOwner.lifecycle.addObserver(observer)
+
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
+    }
 
     BaseScreen(
         hasTopBar = true,
@@ -56,8 +77,12 @@ fun SettingsScreen(onBackButton: () -> Unit) {
                 )
                 Switch(
                     checked = checked,
-                    onCheckedChange = {
-                        checked = it
+                    onCheckedChange = { checked ->
+                        if (checked) {
+                            webViewViewModel.triggerNotificationPermissionRequest()
+                        } else {
+                            webViewViewModel.openNotificationSettings()
+                        }
                     },
                     colors = SwitchDefaults.colors(
                         checkedThumbColor = Color.White,
@@ -73,7 +98,7 @@ fun SettingsScreen(onBackButton: () -> Unit) {
 @Composable
 fun PreviewSettingsScreenLight() {
     AMITheme {
-        SettingsScreen() {}
+        SettingsScreen(viewModel()) {}
     }
 }
 
@@ -81,6 +106,6 @@ fun PreviewSettingsScreenLight() {
 @Composable
 fun PreviewSettingsScreenDark() {
     AMITheme {
-        SettingsScreen() {}
+        SettingsScreen(viewModel()) {}
     }
 }
