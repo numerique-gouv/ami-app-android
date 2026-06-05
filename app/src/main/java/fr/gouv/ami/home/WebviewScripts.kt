dee40d2db819af9a32ef815762fa808c949b4d2f
Aline Bonnet <aline.bonnet@otterways.com>
2026-06-05 11:36:42 +0200

    add getInfos function in JS

---
+++ app/src/main/java/fr/gouv/ami/home/WebviewScripts.kt
@@ -0,0 +1,45 @@
+package fr.gouv.ami.home
+
+import android.content.Context
+import android.util.Log
+import fr.gouv.ami.BuildConfig
+import fr.gouv.ami.R
+
+class WebviewScripts {
+
+    enum class EventWebview(val value: String) {
+        USER_LOGGED_IN("user_logged_in"),
+        USER_LOGGED_OUT("user_logged_out"),
+        NOTIFICATION_PERMISSION_REQUESTED("notification_permission_requested"),
+        NOTIFICATION_PERMISSION_REMOVED("notification_permission_removed");
+
+
+        companion object {
+            fun fromValue(value: String): EventWebview? =
+                entries.find { it.value == value }
+        }
+    }
+
+    companion object {
+        fun nativeInfosScript(
+            context: Context
+        ): String {
+            return """
+        (function() {
+            window.NativeInfos = window.NativeInfos || {};
+
+            window.NativeInfos.getInfos = function() {
+                return {
+                    platform: "android",
+                    app_name: "${context.getString(R.string.app_name)}",
+                    version: "${BuildConfig.VERSION_NAME}",
+                    build: ${BuildConfig.VERSION_CODE},
+                    environment: "${BuildConfig.FLAVOR}",
+                    mode: "${BuildConfig.BUILD_TYPE}"
+                };
+            };
+        })();
+    """.trimIndent()
+        }
+    }
+}
