f65be1b3cbdc49966c36537f320af5b98d4b0019
Aline Bonnet <aline.bonnet@otterways.com>
2026-07-03 10:51:16 +0200

    add write, read and clear function for storage

---
+++ app/src/main/java/fr/gouv/ami/utils/storage/LocalStorageRepositoryProtocol.kt
@@ -0,0 +1,16 @@
+package fr.gouv.ami.utils.storage
+
+abstract class LocalStorageRepositoryProtocol() {
+
+    abstract suspend fun writeBool(key: String, value: Boolean, secureLevel: KeyStoreManager.SecurityLevelType): Boolean
+    abstract suspend fun writeInt(key: String, value: Int, secureLevel: KeyStoreManager.SecurityLevelType): Boolean
+    abstract suspend fun writeString(key: String, value: String, secureLevel: KeyStoreManager.SecurityLevelType): Boolean
+    abstract suspend fun readBool(key: String, secureLevel: KeyStoreManager.SecurityLevelType): Boolean
+    abstract suspend fun readInt(key: String, secureLevel: KeyStoreManager.SecurityLevelType): Boolean
+    abstract suspend fun readString(key: String, secureLevel: KeyStoreManager.SecurityLevelType): Boolean
+    abstract suspend fun deleteBool(key: String, secureLevel: KeyStoreManager.SecurityLevelType): Boolean
+    abstract suspend fun deleteInt(key: String, secureLevel: KeyStoreManager.SecurityLevelType): Boolean
+    abstract suspend fun deleteString(key: String, secureLevel: KeyStoreManager.SecurityLevelType): Boolean
+    abstract suspend fun deleteAll(secureLevel: KeyStoreManager.SecurityLevelType): Boolean
+
+}
