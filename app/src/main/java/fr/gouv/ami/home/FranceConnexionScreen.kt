7e29c49a1e28cb64f601eda0ca6df75f4355ca5c
Aline Bonnet <aline.bonnet@otterways.com>
2026-07-21 16:02:44 +0200

    modify UI for FC screen

--- app/src/main/java/fr/gouv/ami/home/FranceConnexionScreen.kt
+++ app/src/main/java/fr/gouv/ami/home/FranceConnexionScreen.kt
@@ -9,26 +9,25 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Gray
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.LinkAnnotation
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.withLink
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -44,33 +43,45 @@ fun FranceConnexionScreen(onFcClick: () -> Unit) {
     val uriHandler = LocalUriHandler.current
 
     BaseScreen(viewModel = viewModel()) {
-        Box(
+        /*Box(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(16.dp)
-        ) {
-
-            Column(
+        ) {*/
+        Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+            Row(
                 modifier = Modifier
-                    .fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+                    .weight(1f)
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.Bottom,
+                horizontalArrangement = Arrangement.Center
             ) {
                 Image(
                     painter = painterResource(R.drawable.ic_application),
                     contentDescription = "image d'application"
                 )
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
                 Text(
-                    modifier = Modifier.padding(vertical = 16.dp),
-                    text = stringResource(R.string.franceConnexion_description), fontSize = 16.sp
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    textAlign = TextAlign.Center,
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Bold,
+                    text = stringResource(R.string.home_title)
                 )
                 Text(
                     modifier = Modifier.padding(
-                        vertical = 24.dp
+                        bottom = 24.dp
                     ),
                     text = stringResource(R.string.franceConnexion_subtitle),
-                    fontSize = 12.sp,
-                    color = Gray
+                    fontSize = 14.sp
                 )
                 Button(
                     onClick = onFcClick,
@@ -82,24 +93,34 @@ fun FranceConnexionScreen(onFcClick: () -> Unit) {
                     )
                 }
                 TextButton(
-                    onClick = {uriHandler.openUri("https://franceconnect.gouv.fr/")}
-                ) { Text(stringResource(R.string.FC_button_description))}
+                    onClick = { uriHandler.openUri("https://franceconnect.gouv.fr/") }
+                ) {
+                    Text(
+                        fontSize = 14.sp,
+                        textDecoration = TextDecoration.Underline,
+                        text = stringResource(R.string.FC_button_description)
+                    )
+                }
             }
             Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Bottom,
-                horizontalAlignment = Alignment.CenterHorizontally
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Bottom
             ) {
-                Text(stringResource(R.string.connection_difficult))
                 TextButton(
                     onClick = {
                         context.openOrInstallApp(targetTchap)
                     }
                 ) {
-                    Text(stringResource(R.string.tchap))
+                    Text(
+                        fontSize = 16.sp,
+                        textDecoration = TextDecoration.Underline,
+                        text = stringResource(R.string.cannot_connect)
+                    )
                 }
             }
-
         }
     }
 }
