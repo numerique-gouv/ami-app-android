334599ed71d0c849234ecd9d2b2acb7389e5e10c
Aline Bonnet <aline.bonnet@otterways.com>
2026-09-08 16:05:22 +0200

    add icon for import file menu

--- app/src/main/java/fr/gouv/ami/components/ImportFileBottomSheet.kt
+++ app/src/main/java/fr/gouv/ami/components/ImportFileBottomSheet.kt
@@ -1,7 +1,9 @@
 package fr.gouv.ami.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -10,7 +12,9 @@ import androidx.compose.material3.SheetState
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,16 +41,39 @@ fun ImportFileBottomSheet(
                 .fillMaxWidth()
                 .padding(16.dp)
         ) {
-            Text(
+            Row(
                 modifier = Modifier
-                    .clickable { onFileSelected() }
+                    .fillMaxWidth()
                     .padding(bottom = 8.dp),
-                text = stringResource(R.string.download_menu_file))
-            Text(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.dsfr_file_download_line),
+                    contentDescription = "Sélectionner un fichier"
+                )
+                Text(
+                    modifier = Modifier
+                        .clickable { onFileSelected() }
+                        .padding(start = 16.dp),
+                    text = stringResource(R.string.download_menu_file)
+                )
+            }
+            Row(
                 modifier = Modifier
-                    .clickable { onCameraSelected() }
+                    .fillMaxWidth()
                     .padding(vertical = 8.dp),
-                text = stringResource(R.string.download_menu_camera))
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.dsfr_camera_fill),
+                    contentDescription = "Prendre une photo"
+                )
+                Text(
+                    modifier = Modifier
+                        .clickable { onCameraSelected() }
+                        .padding(start = 16.dp),
+                    text = stringResource(R.string.download_menu_camera))
+            }
         }
     }
 }
